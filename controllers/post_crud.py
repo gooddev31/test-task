42fe74c23ad8553e76b347c14433f4274fa1c0d8
gooddev31 <oleh.stelmashchuklv@gmail.com>
2024-06-12 12:53:10 +0300

    add controller for posts

---
+++ controllers/post_crud.py
@@ -0,0 +1,36 @@
+from sqlalchemy.orm import Session
+
+from database.models import Posts
+from schemas.posts import DeletePostResponse, Post, UpdatePost
+
+
+def post_create(db: Session, post: Post):
+    db_post = Posts(text=post.text)
+    db.add(db_post)
+    db.commit()
+    db.refresh(db_post)
+    return db_post
+
+
+def posts_get_all(db: Session):
+    return db.query(Posts).all()
+
+
+def post_get_one(db: Session, id: int):
+    return db.query(Posts).filter_by(id=id).one()
+
+
+def post_update(db: Session, post: UpdatePost):
+    update_query = {Posts.title: post.text}
+    db.query(Posts).filter_by(id=post.id).update(update_query)
+    db.commit()
+    return db.query(Posts).filter_by(id=post.id).one()
+
+
+def post_delete(db: Session, id: int):
+    post = db.query(Posts).filter_by(id=id).all()
+    if not post:
+        return DeletePostResponse(detail="Doesnt Exist")
+    db.query(Posts).filter_by(id=id).delete()
+    db.commit()
+    return DeletePostResponse(detail="Post Deleted")
--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+from fastapi_jwt_auth import AuthJWT
+from fastapi_jwt_auth.exceptions import AuthJWTException
+from pydantic import BaseModel
+
+from routes.users import router as user_router
 
 app = FastAPI()
 
@@ -12,6 +18,22 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+class Settings(BaseModel):
+    authjwt_secret_key: str = "secret"
+
+@AuthJWT.load_config
+def get_config():
+    return Settings()
+
+@app.exception_handler(AuthJWTException)
+def authjwt_exception_handler(request: Request, exc: AuthJWTException):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"detail": exc.message}
+    )
+
+app.include_router(user_router)
 
 @app.get("/")
 async def index():
