aabf93752b4ba9cda7ef177109a07583f0e4776a
gooddev31 <oleh.stelmashchuklv@gmail.com>
2024-06-12 12:34:13 +0300

    update db configuration

--- database/models.py
+++ database/models.py
@@ -1,7 +1,4 @@
-import uuid
-
-from sqlalchemy import Column, String
-from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy import Column, String, Integer
 
 from database.connection import Base, engine
 
@@ -9,7 +6,7 @@ from database.connection import Base, engine
 class Posts(Base):
     __tablename__ = "posts"
 
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     title = Column(String)
     description = Column(String)
 
