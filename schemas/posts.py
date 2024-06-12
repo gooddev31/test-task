adf647821ce4134b6e187b89ca554ddd0afc304d
gooddev31 <oleh.stelmashchuklv@gmail.com>
2024-06-12 12:51:13 +0300

    add basic user and posts routes and schemas

---
+++ schemas/posts.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel
+
+class Post(BaseModel):
+    id: int
+    text: str
+
+    class Config:
+        orm_mode = True
+
+
+class DeletePostResponse(BaseModel):
+    detail: str
+
+
+class UpdatePost(BaseModel):
+    id: int
+    text: str
+
+    class Config:
+        orm_mode = True
---
+++ schemas/user.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+
+class User(BaseModel):
+    id: int
+    email: str
+
+    class Config:
+        orm_mode = True
+
+class CreteUser(BaseModel):
+    email: str
+    password: str
