698674bcd19999dac23e7fd8bda4acd2dbbb2dd7
gooddev31 <oleh.stelmashchuklv@gmail.com>
2024-06-12 12:38:17 +0300

    update `models.py`

--- database/models.py
+++ database/models.py
@@ -1,14 +1,24 @@
-from sqlalchemy import Column, String, Integer
-
+from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.orm import relationship
 from database.connection import Base, engine
 
+class Users(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    email = Column(String)
+    password = Column(String)
+
+    posts = relationship("Posts", back_populates="user")
 
 class Posts(Base):
     __tablename__ = "posts"
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    title = Column(String)
-    description = Column(String)
+    text = Column(String)
+
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    user = relationship("Users", back_populates="posts")
 
 
 Base.metadata.create_all(engine)
